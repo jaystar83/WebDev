58a2f88e43dcffba9a60895cb0e6f9f618f4c6f1
jaystar <jaystar83@hotmail.de>
2024-06-08 14:32:51 +0200

    3_15 Reaktionstester

--- Learning/WebDevCourse/MySites/3_JS/3_15_Reaktionstester.js
+++ Learning/WebDevCourse/MySites/3_JS/3_15_Reaktionstester.js
@@ -4,75 +4,72 @@ var size_max = 250;
 var formColor_min = 0;
 var formColor_max = 220;
 
-var pgb_size = 500;
+var pgb_size = 450;
 var pgb_left_min = 10;    // links
 var pgb_left_max = pgb_left_min + pgb_size;   // rechts
-//var pgb_top_min = 155;     // oben
-var pgb_top_min = 180;     // oben
+var pgb_top_min = 155;     // oben
 var pgb_top_max = pgb_top_min + pgb_size;//  + size_max;   // unten
+
+var startTime = new Date();
+// CODE, der gemessen werden soll
+var minTime = new Date();
 
 // border around playground ////////////////////////////////////////////////////////////////
 function setPlayground()
 {
-document.getElementById("playground").style.position = "absolute";
-document.getElementById("playground").style.left = pgb_left_min-3+"px";
-document.getElementById("playground").style.top = pgb_top_min-3+"px";
-document.getElementById("playground").style.width = pgb_size+size_max+2+"px";
-document.getElementById("playground").style.height = pgb_size+size_max+2+"px";
-document.getElementById("playground").style.border = "2px black solid";
+	document.getElementById("playground").style.position = "absolute";
+	document.getElementById("playground").style.left = pgb_left_min-3+"px";
+	document.getElementById("playground").style.top = pgb_top_min-3+"px";
+	document.getElementById("playground").style.width = pgb_size+size_max+2+"px";
+	document.getElementById("playground").style.height = pgb_size+size_max+2+"px";
+	document.getElementById("playground").style.border = "2px black solid";
 }
 // random values    ////////////////////////////////////////////////////////////////////////
 function setRandomNewForm()
 {
-var posiLeft = Math.floor(Math.random() * (pgb_left_max - pgb_left_min + 1)) + pgb_left_min;
-var posiTop = Math.floor(Math.random() * (pgb_top_max - pgb_top_min + 1)) + pgb_top_min;
+	var posiLeft = Math.floor(Math.random() * (pgb_left_max - pgb_left_min + 1)) + pgb_left_min;
+	var posiTop = Math.floor(Math.random() * (pgb_top_max - pgb_top_min + 1)) + pgb_top_min;
 
-var size = Math.floor(Math.random() * (size_max - size_min + 1)) + size_min;
+	var size = Math.floor(Math.random() * (size_max - size_min + 1)) + size_min;
+	var red =  Math.floor(Math.random() * (formColor_max - formColor_min + 1)) + formColor_min;
+	var blue = Math.floor(Math.random() * (formColor_max - formColor_min + 1)) + formColor_min;
+	var yellow =  Math.floor(Math.random() * (formColor_max - formColor_min + 1)) + formColor_min;
 
-// test
-//posiLeft = pgb_left_max;
-//posiTop =  pgb_top_max;
-//size = size_max;
-//posiLeft = pgb_left_min;
-//posiTop =  pgb_top_min;
-//size = size_max;
+	var formColor = `rgb(${red},${blue},${yellow})`;
 
-var red =  Math.floor(Math.random() * (formColor_max - formColor_min + 1)) + formColor_min;
-var blue = Math.floor(Math.random() * (formColor_max - formColor_min + 1)) + formColor_min;
-var yellow =  Math.floor(Math.random() * (formColor_max - formColor_min + 1)) + formColor_min;
+	var look = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
 
-var formColor = `rgb(${red},${blue},${yellow})`;
+	// border around playground ////////////////////////////////////////////////////////////////
+	document.getElementById("formToClick").style.left = posiLeft+"px";
+	document.getElementById("formToClick").style.top = posiTop+"px";
 
-var look = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-
-// border around playground ////////////////////////////////////////////////////////////////
-document.getElementById("formToClick").style.left = posiLeft+"px";
-document.getElementById("formToClick").style.top = posiTop+"px";
-
-document.getElementById("formToClick").style.width = size+"px";
-document.getElementById("formToClick").style.height = size+"px";
-if( look == 1)
-    document.getElementById("formToClick").style.borderRadius = size/2+"px";
-document.getElementById("formToClick").style.backgroundColor = formColor;
-document.getElementById("formToClick").style.position = "absolute";
-document.getElementById("formToClick").style.left = posiLeft+"px";
-document.getElementById("formToClick").style.top = posiTop+"px";
+	document.getElementById("formToClick").style.width = size+"px";
+	document.getElementById("formToClick").style.height = size+"px";
+	if( look == 1)
+		document.getElementById("formToClick").style.borderRadius = (size/2)+"px";
+	document.getElementById("formToClick").style.backgroundColor = formColor;
+	document.getElementById("formToClick").style.position = "absolute";
+	document.getElementById("formToClick").style.left = posiLeft+"px";
+	document.getElementById("formToClick").style.top = posiTop+"px";
 }
+
 setPlayground();
 setRandomNewForm();
 
 
-document.getElementById("formToClick").onClick = function()
+document.getElementById("formToClick").onclick = function ()
 {
-	alert("klick");
 	setRandomNewForm();
+	// CODE, der gemessen werden soll
+	var elapsedTime = new Date() - startTime;
+	// time is the number of milliseconds it takes to execute the script
+	startTime = new Date();
+	if(elapsedTime < minTime)
+		minTime = elapsedTime;
+	document.getElementById("elapsedTime").innerHTML = "Benötigte Zeit: " +	elapsedTime + "ms; Schnellste: " + minTime + "ms";
+
 }
 
-document.getElementById("einButton").onClick = function()
-{
-	alert("klick");
-	//setRandomNewForm();
-}
 
 
 
